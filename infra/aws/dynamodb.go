3935042fe9d628fcfeba67d30c607bc70bb3716a
sweeneyf <frankjsweeney2308@gmail.com>
2022-04-13 21:56:27 +0100

    basic write to dynamodb

--- infra/aws/dynamodb.go
+++ infra/aws/dynamodb.go
@@ -1,6 +1,13 @@
 package aws
 
 import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/google/uuid"
 	"github.com/sweeneyf/seal-it-api/entity"
 	"github.com/sweeneyf/seal-it-api/pkg/config"
 	"github.com/sweeneyf/seal-it-api/pkg/logger"
@@ -19,5 +26,30 @@ func NewDynamoDbClient(region string) *DynamoDbClient {
 func (h *DynamoDbClient) SaveDeed(config config.Configuration, deed entity.Deed) error {
 
 	logger.Log.Info("Saving deed to dynamodb")
+	cfg, err := awsconfig.LoadDefaultConfig(context.TODO(), func(o *awsconfig.LoadOptions) error {
+		o.Region = config.Cloud.Region
+		return nil
+	})
+	if err != nil {
+		logger.Log.Error(err.Error(), err)
+		return err
+	}
+
+	// Create an Amazon S3 service client
+	svc := dynamodb.NewFromConfig(cfg)
+	_, err = svc.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: aws.String("deed"),
+		Item: map[string]types.AttributeValue{
+			"id":   &types.AttributeValueMemberS{Value: uuid.New().String()},
+			"deed": &types.AttributeValueMemberS{Value: "test"},
+			"type": &types.AttributeValueMemberS{Value: "test"},
+		},
+	})
+	if err != nil {
+		logger.Log.Error(err.Error(), err)
+		return err
+	}
+	logger.Log.Info("Deed saved")
+
 	return nil
 }
